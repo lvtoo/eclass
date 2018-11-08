51ce52526fcc3618f80e1ed0046a09696a81116a
lvtoo <o@oouul.com>
2018-11-09 00:04:59 +0800

    Data数据单独提取，改为引用

---
+++ eclass/data.py
@@ -0,0 +1,8 @@
+mysql = {
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'oouul',
+        'PASSWORD': 'zhoupengfei',
+        'HOST': '120.79.80.72',
+        'PORT': 3306,
+}
--- eclass/settings.py
+++ eclass/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'eclass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': data.mysql['ENGINE'],
+        'NAME': data.mysql['NAME'],
+        'USER': data.mysql['USER'],
+        'PASSWORD': data.mysql['PASSWORD'],
+        'HOST': data.mysql['HOST'],
+        'PORT': data.mysql['PORT'],
     }
 }
 
