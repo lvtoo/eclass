e8800db8dfc220ffdfeb6aa931cc26d1c5aa311e
lvtoo <o@oouul.com>
2018-11-10 21:21:11 +0800

    增加了templates目录，

--- eclass/settings.py
+++ eclass/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eclass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- eclass/urls.py
+++ eclass/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+d
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ),
 ]
