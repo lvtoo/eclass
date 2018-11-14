f813aebd6ebbf03f742333b2273aa92cd14d57dd
lvtoo <o@oouul.com>
2018-11-14 23:40:24 +0800

    insert home
    Signed-off-by: lvtoo <o@oouul.com>

--- home/views.py
+++ home/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {'title': "ss", 'p': 11}
-    return render(request, index, context)
+    context = {'code': 200,
+               'title': "海大资讯",
+               'p': '功能正在开发~敬请期待'}
+    return render(request, 'index.html', context)
