2d0f37628f05b79404def828c79258260d7b90f4
lvtoo <o@oouul.com>
2018-11-17 22:13:26 +0800

    添加爬虫 manage控制
    Signed-off-by: lvtoo <o@oouul.com>

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class News(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    type = models.CharField(max_length=20)
+    public = models.CharField(max_length=20, null=True)
+    display = models.BooleanField(default=False)
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    source = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
