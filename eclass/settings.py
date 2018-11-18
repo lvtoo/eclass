7e214f2abe8ac10df415729787f895b4208a1ccd
lvtoo <o@oouul.com>
2018-11-18 18:36:14 +0800

    允许 所以地址访问
    Signed-off-by: lvtoo <o@oouul.com>

--- eclass/settings.py
+++ eclass/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$h8u+hk)u!duw!p6p6&mgm-e+^5hz0%2#++sg97&5^5=vt%c)i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
