34f512f9d8d88b1066ff6bbe9e05e8090491af39
lvtoo <o@oouul.com>
2018-11-21 23:25:51 +0800

    更新 views 添加了一套describe
    Signed-off-by: lvtoo <o@oouul.com>

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class New(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
+    describe = models.CharField(max_length=200, null=True)
     text = models.TextField()
     type = models.CharField(max_length=20)
     public = models.CharField(max_length=20, null=True)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from home.models import New
+import json
 
 
 # Create your views here.
 def index(request):
-    content = {'code': 200,
-               'title': "海大资讯",
-               'p': '功能正在开发~敬请期待'}
-    return render(request, 'home/index.html', content)
+    content = {}
+    new = New.objects.filter(display=True).values('title', 'describe', 'pub_date')[0:10]
+    for i in range(0, len(new)):
+        content.update({i: new[i]})
+    return render(request, 'home/index.html', content, status=200)
