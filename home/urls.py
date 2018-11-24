95d4b3527806b0bcce5f85dc5d982b8599311bda
jsz <jsz14897502@163.com>
2018-11-25 00:47:34 +0800

    自动加载已完成
    Signed-off-by: jsz <jsz14897502@163.com>

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('api/news', views.getdata, name='index_api'),
+    path('api/news', views.IndexApi.as_view(), name='index_api'),
     path('detail/<news_id>', views.NewsDetailView.as_view(), name='news_detail'),
 ]
