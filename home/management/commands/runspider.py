346024dff3a1b48ae6248215c0a3586084a5727a
lvtoo <o@oouul.com>
2018-11-24 12:55:57 +0800

    更改爬虫名字
    Signed-off-by: lvtoo <o@oouul.com>

--- home/management/commands/runspider.py
+++ home/management/commands/runspider.py
@@ -5,6 +5,9 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        import home.robot.robot_jwc_info
+        # 启动 教务处新闻
+        import home.robot.robot_jwc_news_info
+        # 启动 讲座通知
         import home.robot.robot_note_info
+
     pass
