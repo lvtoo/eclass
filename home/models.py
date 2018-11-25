cd5a1c777a097a48939388d78a07d6defb883338
lvtoo <o@oouul.com>
2018-11-25 17:48:52 +0800

    models增加一条img_url
    Signed-off-by: lvtoo <o@oouul.com>

--- home/models.py
+++ home/models.py
@@ -5,13 +5,14 @@ from django.utils import timezone
 class New(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    describe = models.CharField(max_length=200, null=True)
+    describe = models.CharField(max_length=200, null=True, blank=True)
     text = models.TextField()
     type = models.CharField(max_length=20)
-    public = models.CharField(max_length=20, null=True)
+    public = models.CharField(max_length=20, null=True, blank=True)
     display = models.BooleanField(default=False)
     pub_date = models.DateTimeField('date published')
     source = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=300, null=True, blank=True)
 
     # class Meta:
     #     ordering:['-pub_date']
