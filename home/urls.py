266c2b999c8966a85a908b74d7acf35623e9a6a9
lvtoo <o@oouul.com>
2018-12-18 22:29:47 +0800

    更新 display显示，搜索功能，访问浏览量
    Signed-off-by: lvtoo <o@oouul.com>

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('api/news', views.IndexApi.as_view(), name='index_api'),
-    path('detail/<news_id>', views.NewsDetailView.as_view(), name='news_detail'),
+    path('search/<str:search_key>', views.search_new, name='search'),
+    path('detail/<int:news_id>', views.NewsDetailView.as_view(), name='news_detail'),
 ]
