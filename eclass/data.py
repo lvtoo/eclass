0b43e05317965312909f369b164dc64f58dc6e3f
lvtoo <o@oouul.com>
2019-04-15 15:34:24 +0800

    添加数据库配置文件

---
+++ eclass/data.py
@@ -0,0 +1,8 @@
+mysql = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'eclass_test',
+    'USER': 'root',
+    'PASSWORD': 'yourPassword',
+    'HOST': '127.0.0.1',
+    'PORT': 3306,
+}
