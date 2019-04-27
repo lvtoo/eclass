1a524ad14e6fd4afa141ccbfcd3a701c5091ac04
lvtoo <o@oouul.com>
2019-04-27 14:54:18 +0800

    更新校园动态爬虫，作者 孔晨

--- home/robot/robot_campus_news_info.py
+++ home/robot/robot_campus_news_info.py
@@ -1,50 +1,35 @@
-"""
-Version: 0.1
-Author: lvtoo
-e-mail: o@oouul.com
-Date: 2018/11/25
-
-"""
 import requests
 from bs4 import BeautifulSoup
-from home.models import New
 from datetime import datetime
+from home.models import New
 
-
-def del_start_blank(str1):
-    for i in range(100):
-        if str1.startswith('       '):
-            str1 = str1[7:]
-        else:
-            return str1
-
-
-times = 0
-url = 'https://www.shmtu.edu.cn'
-r = requests.get(url + '/news')
-soup = BeautifulSoup(r.content, 'lxml')
-div_tag = soup.find('div', class_='view-content')
-all_li = div_tag.find_all('li')
-for li in all_li:
-    a = li.find('a')
-    title = a.string
-    source = url + a['href']
-    pub_date = li.find('span', class_='date-display-single')['content'][:10]
-    pub_date = datetime.strptime(pub_date, '%Y-%m-%d')
-    r = requests.get(source)
+url = 'http://www.shmtu.edu.cn'
+r = requests.get(url + '/news')  # 获取校园动态网页代码
+soup = BeautifulSoup(r.content, 'lxml')  # 用lxml进行解析
+div_tag = soup.find('div', class_='view-content')  # 找出指定class类型的div标签
+all_li = div_tag.find_all('li')  # 找出li标签，为列表格式
+times = 0  # 计数器
+for li in all_li:  # 逐一分析
+    a = li.find('a')  # 找出a标签
+    title = a.string  # 获取链接及标题名
+    source = url + a['href']  # 具体动态内容网址
+    s = li.find('span', class_='date-display-single')  # 找出指定class类型span标签
+    pub_date = s['content'][:10]  # 公布时间为s的content属性前10位
+    pub_date = datetime.strptime(pub_date, "%Y-%m-%d")  # 转格式
+    r = requests.get(source)  # 获取动态内容网页
     soup = BeautifulSoup(r.content, 'lxml')
-    div_tag = soup.find_all('div', class_='region region-content')
-    text = div_tag.text
-    text = del_start_blank(text)
-    describe = text[:70]
-    try:
+    div_tag = soup.find('div', class_='region region-content')
+    text = div_tag.text  # text为div块内文本
+    describe = text[:70]  # describe为text前70位
+    try:  # 尝试寻找动态来源，没有的话保存空字符
         img_src = div_tag.find('img')['src']
     except TypeError:
-        img_src = ''
-    obj = New.objects.filter(title=title)
-    if not obj:
-        new = New(title=title, public='SMU', source=source, text=text, type='news', pub_date=pub_date,
-                  describe=describe, img_url=img_src)
+        img_src = ' '
+    obj = New.objects.filter(title=title)  # 过滤标题
+    if not obj:  # 如果数据库中没有则保存
+        new = New(title=title, describe=describe, text=text, type='new', public='SMU', pub_date=pub_date, source=source,
+                  img_url=img_src)
         new.save()
         times += 1
-print("已更新" + str(times) + "条校园动态。")
+    # print(title,describe,source)#试验
+print('更新数' + str(times) + '校园动态')  # 显示更新条数
